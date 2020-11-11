c809e8f3c8d518faea097be6af5223484535955a
vidyawhitehatjr <vidyalakshmi.vibha@gmail.com>
2020-11-11 20:06:23 +0530

    platform added

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg;
+var platform;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -17,6 +18,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
+    platform = new Ground(150,300,300,170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -34,6 +36,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+
 
 }
 
@@ -59,4 +62,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
 }
